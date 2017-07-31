0964e71917c8d8f933c234b38ae0d64f87b190de
Loïc Renaud <loic1.renaud@hotmail.fr>
2017-07-31 11:50:33 +0200

    palindromeRearranging

---
+++ CodeFights/palindromeRearranging.js
@@ -0,0 +1,15 @@
+function palindromeRearranging(a) {
+    let map = {};
+    let sum = 0;
+
+    for(let i = 0; i<a.length; i++) {
+        sum += ((map[a[i]] = -~map[a[i]])%2) *2 - 1;
+    }
+    return (sum == 0 || sum == 1);
+}
+// Given a string, find out if its characters can be rearranged to form a palindrome.
+// Example
+// For inputString = "aabb", the output should be
+// palindromeRearranging(inputString) = true.
+//
+// We can rearrange "aabb" to make "abba", which is a palindrome.
