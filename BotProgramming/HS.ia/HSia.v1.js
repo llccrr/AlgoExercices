8734b7317ffcbb297387dd60d477f275a45c8844
Thomas Berthaud <tomberthaud@gmail.com>
2018-08-24 01:09:35 +0200

    fix refacto bug

--- BotProgramming/HS.ia/HSia.v1.js
+++ BotProgramming/HS.ia/HSia.v1.js
@@ -1,138 +1,4 @@
 ////////// DECKS ///////////////////////////////////////////////////////////////////////////////////////////////////////
-const scale = {
-  1: 2,
-  2: 0.75,
-  3: 2.5,
-  4: 0,
-  5: 2.5,
-  6: 2,
-  7: 2,
-  8: 2,
-  9: 3,
-  10: 0.75,
-  11: 1.5,
-  12: 2,
-  13: 2.5,
-  14: 0,
-  15: 2,
-  16: 0.75,
-  17: 2,
-  18: 2.5,
-  19: 2.5,
-  20: 0.5,
-  21: 2,
-  22: 1.5,
-  23: 2.5,
-  24: 0.5,
-  25: 0.75,
-  26: 2.5,
-  27: 2,
-  28: 2.5,
-  29: 3,
-  30: 3,
-  31: 0.5,
-  32: 3.5,
-  33: 3,
-  34: 2.5,
-  35: 2,
-  36: 4,
-  37: 3.5,
-  38: 2.5,
-  39: 2.5,
-  40: 3,
-  41: 2.5,
-  42: 2,
-  43: 3,
-  44: 5,
-  45: 4,
-  46: 2.5,
-  47: 2.5,
-  48: 3,
-  49: 4.5,
-  50: 3,
-  51: 3.5,
-  52: 4,
-  53: 4.5,
-  54: 3.5,
-  55: 2.5,
-  56: 2,
-  57: 0.75,
-  58: 2,
-  59: 1.5,
-  60: 0.75,
-  61: 2,
-  62: 2.5,
-  63: 2.5,
-  64: 3,
-  65: 3,
-  66: 2,
-  67: 3.5,
-  68: 3,
-  69: 4.5,
-  70: 2.5,
-  71: 1.5,
-  72: 2,
-  73: 3,
-  74: 3,
-  75: 2,
-  76: 3,
-  77: 0.75,
-  78: 0.5,
-  79: 0.75,
-  80: 4.5,
-  81: 2,
-  82: 2.5,
-  83: 0,
-  84: 3,
-  85: 2.5,
-  86: 2,
-  87: 2.5,
-  88: 2,
-  89: 2,
-  90: 0.75,
-  91: 2,
-  92: 0.5,
-  93: 2.5,
-  94: 2.5,
-  95: 3,
-  96: 2.5,
-  97: 2.5,
-  98: 2.5,
-  99: 3,
-  100: 2,
-  101: 3,
-  102: 2,
-  103: 3.5,
-  104: 2.5,
-  105: 3,
-  106: 2.5,
-  107: 2,
-  108: 1,
-  109: 1.5,
-  110: 0.75,
-  111: 3,
-  112: 2,
-  113: 0.5,
-  114: 2,
-  115: 2.5,
-  116: 3,
-  120: 3,
-  122: 2.5,
-  123: 2.5,
-  129: 3.5,
-  133: 2.5,
-  135: 3.5,
-  139: 5,
-  141: 2.5,
-  144: 4,
-  150: 4,
-  151: 5,
-  152: 3.5,
-  155: 3.5,
-  158: 5,
-  159: 2
-};
-
 const zooDeck = {
   0: 0,
   1: 3,
@@ -152,13 +18,13 @@ const zooDeck = {
       breakthrough: 1,
       charge: 1,
       drain: 1,
-      guard: 4,
+      guard: 5,
       lethal: 12,
-      ward: 5
+      ward: 3
     };
     const cost = 1.5;
 
-    let value = (card.attack * attack + card.defense * defense) / (card.cost || 1) * cost;
+    let value = ((card.attack * attack + card.defense * defense) / (card.cost || 1)) * cost;
     value *= Object.keys(abilities).reduce((total, ability) => total * (card[ability] ? abilities[ability] : 1), 1);
     value *= card.attack !== 0;
     return value;
@@ -174,8 +40,8 @@ const greenDeck = {
   6: 0,
   7: 2,
   'green item': 0,
-  'red item': 8,
-  'blue item': 8,
+  'red item': 0,
+  'blue item': 0,
   value: card => ([144, 150, 158, 151, 152].includes(card.id) ? 10000 : 0)
 };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -236,10 +102,6 @@ class Combo {
 
   isPlayable() {
     return !this.opponent.downed && !this.combo.some(card => card.used);
-  }
-
-  formatActions() {
-    this.combo.map(card => Actions.attack(card, this.opponent)).join(';');
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -331,58 +193,36 @@ function getCardsList() {
   return cards;
 }
 
-function draft(cards) {
-  // log('player', player);
-  // log('cards', cards);
-  cards = cards.map((card, index) => {
-    card.draftId = index;
-    return card;
-  });
+function draft(cards, turn) {
+  const pickBestCard = cards => cards.sortBy('value', 'desc')[0];
 
-  cards.forEach(card => {
-    if (!Object.keys(scale).includes(card.cardNumber.toString())) {
-      //log(`card ${card.number} not in the scale`);
-      card.score = -1;
-    } else {
-      card.score = scale[card.cardNumber]; // + curve[card.cost] * player.deck / 60;
-      //debug('score : ', card.score);
+  const pickCurvedCard = cards => {
+    const sortedByBest = cards.sortBy('value', 'desc');
+    log(...sortedByBest.map(card => `${card.attack} ${card.defense} pour ${card.cost} (${card.value})`));
+
+    let chosenCard = sortedByBest.find(card => card.deck()[card.cost] > 0 && card.deck()[card.cardType] > 0);
+    if (!chosenCard) {
+      chosenCard = sortedByBest.find(card => card.deck()[card.cardType] > 0);
     }
-  });
-  cards.sort((card1, card2) => card2.score - card1.score);
-  //curve[cards[0].cost]--;
-  return cards[0];
-  //pick(cards[0].draftId);
+    if (!chosenCard) {
+      chosenCard = sortedByBest.find(card => card.deck()[card.cost] > 0);
+    }
+
+    const cardToPick = chosenCard || sortedByBest[0];
+    cardToPick.deck()[cardToPick.cost]--;
+    cardToPick.deck()[cardToPick.cardType]--;
+    log(cardToPick.deck());
+    return cardToPick;
+  };
+
+  let card;
+  if (turn <= 12) {
+    card = pickCurvedCard(cards);
+  } else {
+    card = pickCurvedCard(cards);
+  }
+  return card;
 }
-// function draft(cards, turn) {
-//   const pickBestCard = cards => cards.sortBy('value', 'desc')[0];
-//
-//   const pickCurvedCard = cards => {
-//     const sortedByBest = cards.sortBy('value', 'desc');
-//     log(...sortedByBest.map(card => `${card.attack} ${card.defense} pour ${card.cost} (${card.value})`));
-//
-//     let chosenCard = sortedByBest.find(card => card.deck()[card.cost] > 0 && card.deck()[card.cardType] > 0);
-//     if (!chosenCard) {
-//       chosenCard = sortedByBest.find(card => card.deck()[card.cardType] > 0);
-//     }
-//     if (!chosenCard) {
-//       chosenCard = sortedByBest.find(card => card.deck()[card.cost] > 0);
-//     }
-//
-//     const cardToPick = chosenCard || sortedByBest[0];
-//     cardToPick.deck()[cardToPick.cost]--;
-//     cardToPick.deck()[cardToPick.cardType]--;
-//     log(cardToPick.deck());
-//     return cardToPick;
-//   };
-//
-//   let card;
-//   if (turn <= 12) {
-//     card = pickCurvedCard(cards);
-//   } else {
-//     card = pickCurvedCard(cards);
-//   }
-//   return card;
-// }
 
 function summonCreatures(myHand, myBoard) {
   const sortedByCost = myHand.filter(card => card.cardType === 'creature' && card.cost <= me.playerMana).sortBy('cost');
@@ -419,7 +259,7 @@ function summonCreatures(myHand, myBoard) {
     const currentCost = currentCombo.reduce((total, card) => total + card.cost, 0);
     return bestCombo.length + bestCost < currentCombo.length + currentCost ? currentCombo : bestCombo;
   });
-  log('Best Summoning combo', comboToSummon);
+  //log('Best Summoning combo', comboToSummon);
 
   //add directly to the board all cards with charge
   comboToSummon.forEach(card => {
@@ -453,7 +293,7 @@ function getDangerosity(crea) {
     danger += 5 * crea.ward;
     danger += 3 * crea.guard;
   } else {
-    danger += Math.sqrt(crea.attack) * Math.sqrt(crea.defense) / 2;
+    danger += (Math.sqrt(crea.attack) * Math.sqrt(crea.defense)) / 2;
     danger += 1.5 * crea.attack * (1.5 + crea.ward);
     danger += 1.5 * crea.defense * (1.5 + crea.guard);
   }
@@ -478,20 +318,20 @@ function playRemovals(myHand, opponentBoard) {
     const danger = getDangerosity(opponent);
     const chosen = removals.sort((prev, next) => getRedValue(next) - getRedValue(prev)).find(removal => {
       const redValue = getRedValue(removal);
-      log('danger' + danger + ' ' + 'redValue' + redValue);
+      //log('danger' + danger + ' ' + 'redValue' + redValue);
       if (redValue < danger) {
-        log('target legit', opponent.id);
+        //log('target legit', opponent.id);
         if (removal.defense + opponent.defense <= 0 && (!opponent.ward || removal.ward)) {
-          log('can kill it');
+          //log('can kill it');
           return true;
         } else {
-          log('cant kill it');
-          log(`${opponent.defense} + ${removal.defense} = ${removal.defense + opponent.defense}`);
+          //log('cant kill it');
+          //log(`${opponent.defense} + ${removal.defense} = ${removal.defense + opponent.defense}`);
         }
       }
       return false;
     });
-    log('toKill (red)', chosen);
+    //log('toKill (red)', chosen);
     if (chosen && me.playerMana >= chosen.cost) {
       chosenActions.push(`USE ${chosen.id} ${opponent.id}`);
       me.playerMana -= chosen.cost;
@@ -549,38 +389,33 @@ function attackGuards(myBoard, opponentBoard) {
   ////////////////////////////////////////////////////////////////////////////////////////////////
   const combos = getAllCombos(myBoard, opponentBoard);
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  function chooseBestCombo(combos, opponents) {
+  function chooseBestCombo(combos, opponentBoard) {
     const chosenCombos = [];
-    for (let i = 0; i < opponents.length; i++) {
-      let playableCombos = combos.filter(combo => combo.isPlayable());
-      //freetrades
+    for (let i = 0; i < opponentBoard.length; i++) {
+      let opGuardCombos = combos.filter(combo => !combo.opponent.downed && !combo.combo.some(card => card.used));
+
       let bestCombos,
         sorted = [];
-      const freeTrades = playableCombos
+      const freeTrades = opGuardCombos
         .filter(combo => combo.loss === 0 && combo.efficient <= 0)
         .sort((prev, next) => next.efficient - prev.efficient);
       sorted.push(...freeTrades);
-      //log('freetrades', freeTrades);
 
-      log('playableCombos', playableCombos);
-      const killingCombos = playableCombos.filter(combo => combo.efficient <= 0);
-      const sortedByLoss = (killingCombos.length === 0 ? playableCombos : killingCombos).sort(
+      const killingCombos = opGuardCombos.filter(combo => combo.efficient <= 0);
+      const sortedByLoss = (killingCombos.length === 0 ? opGuardCombos : killingCombos).sort(
         (prev, next) => prev.loss - next.loss
       );
-      log('sortedByLoss', sortedByLoss);
       const sortedByEfficient = sortedByLoss
         .filter(combo => combo.loss === sortedByLoss[0].loss)
         .sort((prev, next) => next.efficient - prev.efficient);
-      log('efficientcombos', sortedByEfficient);
-      sorted.push(sortedByEfficient);
+      sorted.push(...sortedByEfficient);
 
-      //between same combos, choose strongest opponent
       bestCombos = sorted
-        .filter(combo => combo.loss === sorted[0].loss && combo.efficient === sorted[0].efficient)
+        .filter(combo => combo.efficient === sorted[0].efficient)
         .sort((prev, next) => next.opponent.attack - prev.opponent.attack);
 
       bestCombos.forEach(combo => {
-        if (combo.isPlayable()) {
+        if (!combo.combo.some(card => card.used)) {
           //eviter de push 2 fois le même combo
           chosenCombos.push(combo);
           combo.opponent.downed = true;
@@ -590,10 +425,11 @@ function attackGuards(myBoard, opponentBoard) {
         }
       });
     }
+    log('chosenCombos', chosenCombos);
     return chosenCombos;
   }
   const chosen = chooseBestCombo(combos, opponentBoard);
-  return chosen.map(combo => combo.formatActions());
+  return chosen.map(combo => combo.combo.map(card => Actions.attack(card, combo.opponent)).join(';'));
 }
 
 function cantKillPlayer(myBoard) {
@@ -610,20 +446,18 @@ function attack(myBoard, opponentBoard) {
   function chooseBestCombo(combos, opponents) {
     const chosenCombos = [];
     for (let i = 0; i < opponents.length; i++) {
-      let playableCombos = combos.filter(combo => combo.isPlayable());
+      let opCombos = combos.filter(combo => !combo.opponent.downed && !combo.combo.some(card => card.used));
       //freetrades
       let bestCombos,
         sorted = [];
-      const freeTrades = playableCombos
+      const freeTrades = opCombos
         .filter(combo => combo.loss === 0 && combo.efficient <= 0)
         .sort((prev, next) => next.efficient - prev.efficient);
       sorted.push(...freeTrades);
       //log('freetrades', freeTrades);
 
       //killing more dangerous cards
-      const killingCombos = playableCombos
-        .filter(combo => combo.efficient <= 0)
-        .sort((prev, next) => prev.loss - next.loss);
+      const killingCombos = opCombos.filter(combo => combo.efficient <= 0).sort((prev, next) => prev.loss - next.loss);
       const efficientsTrades = killingCombos
         .filter(
           combo =>
@@ -632,8 +466,8 @@ function attack(myBoard, opponentBoard) {
             combo.opponent.defense - combo.efficient <= combo.opponent.attack
         )
         .sort((prev, next) => next.efficient - prev.efficient);
+      sorted.push(...efficientsTrades);
       //log('efficientrades', efficientsTrades);
-      sorted.push(efficientrades);
 
       //between same combos, choose strongest opponent
       bestCombos = sorted
@@ -641,7 +475,7 @@ function attack(myBoard, opponentBoard) {
         .sort((prev, next) => next.opponent.attack - prev.opponent.attack);
 
       bestCombos.forEach(combo => {
-        if (combo.isPlayable()) {
+        if (!combo.combo.some(card => card.used)) {
           //eviter de push 2 fois le même combo
           chosenCombos.push(combo);
           combo.opponent.downed = true;
@@ -651,10 +485,14 @@ function attack(myBoard, opponentBoard) {
         }
       });
     }
+    //log('chosenCombos', chosenCombos);
     return chosenCombos;
   }
-  const chosen = chooseBestCombo(combos, opponentBoard);
-  return chosen.map(combo => combo.formatActions());
+  const chosen = chooseBestCombo(combos, opponentBoard).map(combo =>
+    combo.combo.map(card => Actions.attack(card, combo.opponent)).join(';')
+  );
+  //log('attackactions', chosen);
+  return chosen;
 }
 
 function fullFace(myBoard) {
@@ -687,46 +525,6 @@ function getAllCombos(myBoard, opponentBoard) {
     }
   });
   return combos;
-}
-
-function chooseBestActions(combos, opponentBoard, moreTradesFn) {
-  function chooseBestCombo(combos, opponents) {
-    const chosenCombos = [];
-    for (let i = 0; i < opponents.length; i++) {
-      let playableCombos = combos.filter(combo => combo.isPlayable());
-      //freetrades
-      let bestCombos,
-        sorted = [];
-      const freeTrades = playableCombos
-        .filter(combo => combo.loss === 0 && combo.efficient <= 0)
-        .sort((prev, next) => next.efficient - prev.efficient);
-      sorted.push(...freeTrades);
-      //log('freetrades', freeTrades);
-
-      if (moreTradesFn) {
-        sorted.push(...moreTradesFn(playableCombos));
-      }
-
-      //between same combos, choose strongest opponent
-      bestCombos = sorted
-        .filter(combo => combo.loss === sorted[0].loss && combo.efficient === sorted[0].efficient)
-        .sort((prev, next) => next.opponent.attack - prev.opponent.attack);
-
-      bestCombos.forEach(combo => {
-        if (combo.isPlayable()) {
-          //eviter de push 2 fois le même combo
-          chosenCombos.push(combo);
-          combo.opponent.downed = true;
-          combo.combo.forEach(card => {
-            card.used = true;
-          });
-        }
-      });
-    }
-    return chosenCombos;
-  }
-  const chosen = chooseBestCombo(combos, opponentBoard);
-  return chosen.map(combo => combo.formatActions());
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
